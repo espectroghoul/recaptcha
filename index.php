b2c6c151d0df37e28c9caad527adf1ef86351453
espectroghoul <94411828+espectroghoul@users.noreply.github.com>
2021-11-15 20:49:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulário de Reclamação</title>
+    <link rel="stylesheet" href="estilo.css">
+    <script src="https://google.com/recaptcha/api.js"></script>
+</head>
+<body>
+
+    <h1>Página de Reclamação</h1>
+    <div class="corpo">
+        <form action="">
+            <label for="nome">
+                Nome completo:<br>
+                <input type="text" id="nome" name="nome"><br>
+            </label>
+
+            <label for="email">
+                Email:<br>
+                <input type="text" id="email" name="email"><br>
+            </label>
+
+            <label for="reclamação">
+                Escreva sua reclamação abaixo:<br>
+                <textarea name="reclamação" rows="5" cols="50"></textarea>
+            </label><br>
+            <div data-sitekey="6LcvlTgdAAAAANrH-RNARnQgbPscHxnbi_uRTVk3"></div>
+            <input type="submit" value="ENVIAR" id="botão">
+        </form>
+    </div>
+</body>
+</html>
