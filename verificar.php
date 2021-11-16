89666565fa15046be60254989336ddbe759a6a53
espectroghoul <94411828+espectroghoul@users.noreply.github.com>
2021-11-15 21:56:11 -0300

    Update verificar.php

--- verificar.php
+++ verificar.php
@@ -6,10 +6,10 @@ if(!is_null($captcha)){
 	$res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcvlTgdAAAAAN0VxUkohbcdDx7S0AnhlOg8ZXkf&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
 	if($res->success === true){
 		//CAPTCHA validado!!!
-		echo 'Tudo certo =)';
+		echo 'Formulario enviado com sucesso';
 	}
 	else{
-		echo 'Erro ao validar o captcha!!!';
+		echo 'Erro ao validar o captcha, por favor assinale o captcha';
 	}
 }
 else{
